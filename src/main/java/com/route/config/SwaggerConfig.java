241574a1dde11d57b70ce123ff052c9735061215
twarit soni <twarit.s88@gmail.com>
2023-11-21 17:49:53 +0530

    changes in swagger

--- src/main/java/com/route/config/SwaggerConfig.java
+++ src/main/java/com/route/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.route.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,15 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
 	private String schemeName = "bearerScheme";
+	
+	@Value("${server.name}")
+	private String server;
 
 	@Bean
 	public OpenAPI springOpenAPI() {
@@ -25,7 +32,7 @@ public class SwaggerConfig {
 								.scheme("bearer")))
 				.info(new Info().title("Router API").description("").version("v0.0.1")
 						.contact(new Contact().name("Twarit Soni").email("twarit.s88@gmail.com").url("www.hti.com"))
-						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+						.license(new License().name("Apache 2.0").url("http://springdoc.org"))).servers(List.of(new Server().url(server).description("Server")));
 	}
 
 }
